ce888ea1886a29a268c65c2f2e4cf4a8a4aa9d1d
Gena <genada89@gmail.com>
2026-08-24 16:23:12 +0300

    STRANGE commit but Luk want this:)

--- cinema/urls.py
+++ cinema/urls.py
@@ -26,13 +26,13 @@ router = routers.DefaultRouter()
 router.register("movies", MovieViewSet, basename="movie")
 
 urlpatterns = [
-    path("genres/", GenreList.as_view(), name="genre-list"),
-    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
-    path("actors/", ActorList.as_view(), name="actor-list"),
-    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
-    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
-    path("", include(router.urls)),
+    path("api/cinema/genres/", GenreList.as_view(), name="genre-list"),
+    path("api/cinema/genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("api/cinema/actors/", ActorList.as_view(), name="actor-list"),
+    path("api/cinema/actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("api/cinema/cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
+    path("api/cinema/cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
+    path("api/cinema/", include(router.urls)),
 ]
 
 app_name = "cinema"
